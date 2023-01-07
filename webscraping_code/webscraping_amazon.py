820132cb8d50773f6392cd9fa2c237b793e54a6b
camila-marquess <camilamarquesoliveira@outlook.com>
2023-01-07 10:26:04 -0300

    feat: adding function to get products url and info

---
+++ webscraping_code/webscraping_amazon.py
@@ -0,0 +1,59 @@
+#%%
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+headers = {
+    'User-Agent': ('Mozilla/5.0 (X11; Linux x86_64)'
+                    'AppleWebKit/537.36 (KHTML, like Gecko)'
+                    'Chrome/44.0.2403.157 Safari/537.36'),
+    'Accept-Language': 'en-US, en;q=0.5'
+}
+
+#%%
+
+BOOKS = ['1942788339','8576082675']
+
+def get_products_urls(books_list):
+    
+    urls_list = []
+    for book in books_list:
+        url = f"https://www.amazon.com.br/dp/{''.join(book)}/"
+        urls_list.append(url)
+    return urls_list
+
+#%%
+
+def get_responses():
+    lista = get_products_urls(BOOKS)
+    productTitle = []
+    productDescription = []
+    productRating = []
+    productPrice = []
+    
+    for book_url in range(0, len(lista)):
+        response = requests.get(lista[book_url], headers = headers)
+        soup = BeautifulSoup(response.text, "html.parser")
+    
+        product_title = soup.find("span", attrs={"id":'productTitle'}).text.strip()
+        product_description = soup.find("div", attrs={"class":"a-section a-spacing-small a-padding-small"}).text.strip()
+        product_rating = soup.find("span", attrs={"class":"a-icon-alt"}).text.strip()
+        product_price = soup.find("div", attrs={'class': 'a-section aok-hidden twister-plus-buying-options-price-data'}).text.split(", ")
+        
+        for price in product_price:
+            price_dict = json.loads(price) 
+            price_amount = price_dict[0]['priceAmount']
+            
+    
+        productTitle.append(product_title)
+        productDescription.append(product_description)
+        productRating.append(product_rating)
+        productPrice.append(price_amount)
+    return productTitle, productDescription, productRating, productPrice, 
+        
+    
+get_responses()
+
+
+# %%
